87a3a6e9b0276141d30ab921cc592abf310aa7db
maulana04 <tonimaulana429@gmail.com>
2023-10-24 19:20:56 +0700

    dashboard dan deteksi

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -7,7 +7,7 @@
             <div class="row">
                 <div class="col-lg-5">
                     <h1>Selamat Datang</h1>
-                    <p>Di website deteksi dini Benign Prostatic Hyperplasia.</p>
+                    <p>Di Website Deteksi Dini Benign Prostatic Hyperplasia.</p>
                     <a href="<?= site_url('deteksi') ?>" class="template-btn mt-3">Mulai Pendeteksian</a>
                 </div>
             </div>
@@ -21,7 +21,15 @@
                 <div class="col-lg-6 offset-lg-3">
                     <div class="section-top text-center">
                         <h2>Informasi Penyakit</h2>
-                        <p>Green above he cattle god saw day multiply under fill in the cattle fowl a all, living, tree word link available in the service for subdue fruit.</p>
+                        <p>Benign prostatic hyperplasia atau BPH adalah kondisi di mana kelenjar prostat membesar.
+                            Kondisi ini menyebabkan aliran urine menjadi tidak lancar karena penekanan saluran pembuangan urine.
+                            Akibatnya, penderita merasa seperti tidak tuntas setiap buang air kecil dan sering kali harus mengejan untuk dapat mengeluarkan kencing.
+                        <a href="https://www.siloamhospitals.com/informasi-siloam/artikel/apa-itu-bph" class="">read more ...</a>
+                    </p>
+                        <br></br>
+                        <h3>
+                            Berikut ini beberapa gejala penyakit Benign Prostatic Hyperplasia
+                        </h>
                     </div>
                 </div>
             </div>
@@ -36,8 +44,12 @@
                                 </div>
                             </div>
                             <div class="single-department item-padding text-center">
-                                <h3>cardiac clinic</h3>
-                                <p>Hath creeping subdue he fish gred face whose spirit that seasons today multiply female midst upon</p>
+                                <h3>Apa itu Penyakit BPH</h3>
+                                <p>Benign prostatic hyperplasia atau BPH adalah kondisi di mana kelenjar prostat membesar.
+                                    Kondisi ini menyebabkan aliran urine menjadi tidak lancar karena penekanan saluran pembuangan urine.
+                                    Akibatnya, penderita merasa seperti tidak tuntas setiap buang air kecil dan sering kali harus mengejan untuk dapat mengeluarkan kencing.
+                                    <a href="https://www.siloamhospitals.com/informasi-siloam/artikel/apa-itu-bph" class="">read more ...</a>
+                                </p>
                             </div>
                         </div>
                         <div class="single-slide">
@@ -48,8 +60,11 @@
                                 </div>
                             </div>
                             <div class="single-department item-padding text-center">
-                                <h3>plastic surgery</h3>
-                                <p>Hath creeping subdue he fish gred face whose spirit that seasons today multiply female midst upon</p>
+                                <h3>Mengenali Penyebab Penyakit BPH</h3>
+                                <p>Penyebab pasti penyakit Benign Prostatic Hyperplasia (BPH) belum diketahui secara pasti,
+                                    namun hingga saat ini erat dikaitkan dengan proses penuaan yang menyebabkan penurunan  hormon  pria,
+                                    terutama testosteron. Hormon testosteron ini  diubah menjadi dihidrotestosteron (DHT).
+                                    DHT inilah yang  merangsang pembesaran kelenjarnya untuk membesar, yang menutup saluran keluarnya urin</p>
                             </div>
                         </div>
                         <div class="single-slide">
@@ -60,8 +75,16 @@
                                 </div>
                             </div>
                             <div class="single-department item-padding text-center">
-                                <h3>dental clinic</h3>
-                                <p>Hath creeping subdue he fish gred face whose spirit that seasons today multiply female midst upon</p>
+                                <h3>Mengenal Gejala Penyakit BPH</h3>
+                                <p>Gejala umum yang dialami oleh penderita BPH adalah sebagai berikut:</p>
+                                <p>> Meningkatnya frekuensi buang air kecil..
+                                    <br>> Kesulitan buang air kecil hingga harus mengejan.
+                                    <br>> Merasa tidak tuntas setiap buang air kecil.
+                                    <br>> Urine berbau dan warnanya tidak normal.
+                                    <br>> Nyeri saat buang air kecil.
+                                    <br>> Aliran urine melemah dan tersumbat.
+                                    <br>> Perut bagian bawah terasa nyeri dan penuh.
+                                    <br>> Kesulitan menghentikan aliran urine setelah kencing.</p>
                             </div>
                         </div>
                         <div class="single-slide">
@@ -72,8 +95,14 @@
                                 </div>
                             </div>
                             <div class="single-department item-padding text-center">
-                                <h3>cardiac clinic</h3>
-                                <p>Hath creeping subdue he fish gred face whose spirit that seasons today multiply female midst upon</p>
+                                <h3>Pencegahan Penyakit BPH</h3>
+                                <p>Hingga saat ini, belum ada upaya yang bisa dilakukan untuk mencegah terjadinya BPH. Namun, Anda bisa melakukan perawatan mandiri untuk menjaga kesehatan prostat atau mencegah memburuknya gejala BPH, di antaranya:</p>
+                                <p>1. Menghindari asupan air 1–2 jam sebelum tidur.
+                                <br>2. Tidak menunda ataupun menahan buang air kecil.
+                                <br>3. Mengurangi konsumsi alkohol dan kafein.
+                                <br>4. Olahraga secara teratur.
+                                <br>5. Mengatasi stres.                            
+                                <br>6. Menjaga berat badan ideal dan menerapkan pola makan sehat.</p>
                             </div>
                         </div>
                     </div>
--- app/Views/pages/deteksi2.php
+++ app/Views/pages/deteksi2.php
@@ -15,47 +15,163 @@
             <div class="row">
                 <div class="col-lg-10 offset-lg-1 align-self-center">
                     <div class="appointment-form text-center mt-5 mt-lg-0">
-                        <h3 class="mb-5">Diagnosa</h3>
+                        <h3 class="mb-5">Pilih gejala apa yang dirasakan oleh anda</h3>
                         <form action="#">
-                            <div class="form-group text-left">
-                                <label class="label-title-radio">Pertanyaan</label>
-                                <div class="row ml-2">
-                                    <div class="mr-3">
-                                        <input type="radio" id="yes1" name="radio-group" checked>
-                                        <label class="label-radio" for="yes1">Ya</label>
+                            <!-- awal row -->
+                            <div class="row">
+                                <!-- col -->
+                                <div class="col">
+                                <div class ="form-group text-left">
+                                     <label class="label-title-radio">Sering sakit pinggang?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes20" name="radio-group20" >
+                                                <label class="label-radio" for="yes20">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no20" name="radio-group20" checked>
+                                                <label class="label-radio" for="no20">Tidak</label>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <input type="radio" id="no1" name="radio-group">
-                                        <label class="label-radio" for="no1">Tidak</label>
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">Perlu Mengejan saat kencing?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes1" name="radio-group1" >
+                                                <label class="label-radio" for="yes1">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no1" name="radio-group1" checked>
+                                                <label class="label-radio" for="no1">Tidak</label>
+                                            </div>
+                                        </div>
                                     </div>
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">Aliran urin lemah atau tersendat?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes2" name="radio-group2" >
+                                                <label class="label-radio" for="yes2">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no2" name="radio-group2" checked>
+                                                <label class="label-radio" for="no2">Tidak</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">Urin menetes setelah kencing?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes3" name="radio-group3" >
+                                                <label class="label-radio" for="yes3">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no3" name="radio-group3" checked>
+                                                <label class="label-radio" for="no3">Tidak</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">Buang air kencing terasa tidak tuntas?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes4" name="radio-group4" >
+                                                <label class="label-radio" for="yes4">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no4" name="radio-group4" checked>
+                                                <label class="label-radio" for="no4">Tidak</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
-                            </div>
-                            <div class="form-group text-left">
-                                <label class="label-title-radio">Pertanyaan</label>
-                                <div class="row ml-2">
-                                    <div class="mr-3">
-                                        <input type="radio" id="yes2" name="radio-group2" checked>
-                                        <label class="label-radio" for="yes2">Ya</label>
+                                <!-- akhir col -->
+
+                                <!-- awal col -->
+                                <div class="col">
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">kencing di malam hari lebih sering?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes5" name="radio-group5" >
+                                                <label class="label-radio" for="yes5">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no5" name="radio-group5" checked>
+                                                <label class="label-radio" for="no5">Tidak</label>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <input type="radio" id="no2" name="radio-group2">
-                                        <label class="label-radio" for="no2">Tidak</label>
+
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">Sulit menahan ketika ingin kencing?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes6" name="radio-group6" >
+                                                <label class="label-radio" for="yes6">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no6" name="radio-group6" checked>
+                                                <label class="label-radio" for="no6">Tidak</label>
+                                            </div>
+                                        </div>
                                     </div>
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">Nyeri saat kencing?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes7" name="radio-group7" >
+                                                <label class="label-radio" for="yes7">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no7" name="radio-group7" checked>
+                                                <label class="label-radio" for="no7">Tidak</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">terdapat darah dalam urin?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes8" name="radio-group8" >
+                                                <label class="label-radio" for="yes8">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no8" name="radio-group8" checked>
+                                                <label class="label-radio" for="no8">Tidak</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class ="form-group text-left">
+                                     <label class="label-title-radio">Urin tidak keluar sama sekali?</label>
+                                        <div class="row ml-2">
+                                            <div class="mr-3">
+                                                <input type="radio" id="yes9" name="radio-group9" >
+                                                <label class="label-radio" for="yes9">Ya</label>
+                                            </div>
+                                            <div>
+                                                <input type="radio" id="no9" name="radio-group9" checked>
+                                                <label class="label-radio" for="no9">Tidak</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
                                 </div>
+                                <!-- akhir col -->
                             </div>
-                            <div class="form-group text-left">
-                                <label class="label-title-radio">Pertanyaan</label>
-                                <div class="row ml-2">
-                                    <div class="mr-3">
-                                        <input type="radio" id="yes3" name="radio-group3" checked>
-                                        <label class="label-radio" for="yes3">Ya</label>
-                                    </div>
-                                    <div>
-                                        <input type="radio" id="no3" name="radio-group3">
-                                        <label class="label-radio" for="no3">Tidak</label>
-                                    </div>
-                                </div>
-                            </div>
+                            <!-- akhir row -->
+
                             <a href="#" class="template-btn">Selanjutnya</a>
                         </form>
                     </div>
